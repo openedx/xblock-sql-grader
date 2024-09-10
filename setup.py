38e989a3c13bd195c1392084920265405bba4e6e
Feanil Patel <feanil@axim.org>
2024-09-10 11:09:09 -0400

    build: Drop python 3.8 support and add 3.12
    This commit also fixes a bug in the tox.ini file where we weren't
    running the quality tests correctly if you run just `tox`

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ def is_requirement(line):
 
 setup(
     name='xblock-sql-grader',
-    version='0.6.0',
+    version='1.0.0',
     description='SQL Grader XBlock',  # TODO: write a better description.
     license='AGPLv3',
     long_description=README,
@@ -154,7 +154,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ]
 )
